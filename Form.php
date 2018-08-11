6d54dc3c51fa0d9ce24d339ffdd2f19b2902bdb1
Artturitahtinen <tahtinen.artturi@gmail.com>
2018-08-11 17:50:41 +0300

    Editoitu

--- Form.php
+++ Form.php
@@ -46,10 +46,14 @@
         $puhelinnumeroError = "Vaaditaan";
       }else{
       $puhelinnumero = testaaInput($_POST["puhelinnumero"]);
-      if(!preg_match("^((90[0-9]{3})?0|\+358\s?)(?!(100|20(0|2(0|[2-3])|9[8-9])|300|600|700|708|75(00[0-3]|(1|2)\d{2}|30[0-2]|32[0-2]|75[0-2]|98[0-2])))(4|50|10[1-9]|20(1|2(1|[4-9])|[3-9])|29|30[1-9]|71|73|75(00[3-9]|30[3-9]|32[3-9]|53[3-9]|83[3-9])|2|3|5|6|8|9|1[3-9])\s?(\d\s?){4,19}\d$",$puhelinnumero)) {
+      if(!preg_match("/(((90[0-9]{3})?0|\+358\s?)(?!(100|20(0|2(0|[2-3])|9[8-9])|300|600|700|708|75(00[0-3]|(1|2)\d{2}|30[0-2]|32[0-2]|75[0-2]|98[0-2])))(4|50|10[1-9]|20(1|2(1|[4-9])|[3-9])|29|30[1-9]|71|73|75(00[3-9]|30[3-9]|32[3-9]|53[3-9]|83[3-9])|2|3|5|6|8|9|1[3-9])\s?(\d\s?){4,19}\d$)/",$puhelinnumero)) {
       $puhelinnumeroError = "Tarkista numero";
       }
       }
+	  $sähköposti = testaaInput($_POST["sähköposti"]);
+	if(!preg_match("/^$|^.*@.*\..*$/",$sähköposti)){
+	$sähköpostiError = "Tarkista oikeinkirjoitus";	
+	}
       
 
 	  if(empty($_POST["henkilötunnus"]) || (trim($_POST["henkilötunnus"]) == "")){
@@ -102,22 +106,21 @@
 
 	  if(($_POST["työmuodot"]) == "Määräaikainen" && (empty($_POST["aloituspäivämäärä"]))){
 		$aloituspvmError = "Vaaditaan";
-		}else{
+	  }else{
 		$aloituspvm = testaaInput($_POST["aloituspäivämäärä"]);
-    if(!preg_match("/^([0-9]{2}\.[0-9]{2}\.[0-9]{4})$/",$aloituspvm)){
+		if(($_POST["työmuodot"]) == "Määräaikainen" && !preg_match("/^(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})$/",$aloituspvm)){
       $aloituspvmError = "Tarkista oikea muoto";
     }
-		}
+	  }
 
 	  if(($_POST["työmuodot"]) == "Määräaikainen" && (empty($_POST["päättymispäivämäärä"]))){
 		$päättymispvmError = "Vaaditaan";
 		}else{
 		$päättymispvm = testaaInput($_POST["päättymispäivämäärä"]);
-    if(!preg_match("/^([0-9]{2}\.[0-9]{2}\.[0-9]{4})$/",$päättymispvm)){
+		if(($_POST["työmuodot"]) == "Määräaikainen" && !preg_match("/^(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})$/",$päättymispvm)){
       $päättymispvmError = "Tarkista oikea muoto";
-    }
 		}
-
+		}
 	  if(($_POST["työmuodot"]) == "Määräaikainen" && ((empty($_POST["aloituspäivämäärä"])) &&
 	  (empty($_POST["päättymispäivämäärä"])))){
 		$aloituspvmError = "Vaaditaan";
@@ -125,12 +128,23 @@
 		}else{
 		$aloituspvm = testaaInput($_POST["aloituspäivämäärä"]);
 		$päättymispvm = testaaInput($_POST["päättymispäivämäärä"]);
-    if(!preg_match("/^([0-9]{2}\.[0-9]{2}\.[0-9]{4})$/",$aloituspvm) && (!preg_match("/^([0-9]{2}\.[0-9]{2}\.[0-9]{4})$/",$päättymispvm))){
+	  if(($_POST["työmuodot"]) == "Määräaikainen" && !preg_match("/^(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})$/",$aloituspvm) && (!preg_match("/^(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})$/",$päättymispvm))){
       $aloituspvmError = "Tarkista oikea muoto";
       $päättymispvmError = "Tarkista oikea muoto";
-    }
-		}
-    }
+	  }
+	}
+	if($etunimiError == "" && $sukunimiError == "" && 
+	$puhelinnumeroError == "" && $henkilötunnusError == ""
+	&& $asuinpaikkaError == "" && $työnimikeError == ""
+	&& $esimiesError == "" && $sijaintiError == ""
+	&& $aloituspvmError == "" && $päättymispvmError == ""){
+	echo "Moro";	
+	}	
+		
+		
+	}
+	
+    
     function testaaInput($data){
       $data = trim($data);
       $data = stripslashes($data);
@@ -138,6 +152,19 @@
       return $data;
     }
     ?>
+	
+	<?php
+	$servername = "localhost";
+	$username = "Arke";
+	$password = "TietoK96";
+	
+	$connection = new mysqli($servername, $username, $password);
+	
+	if($connection ->connect_error){
+	die("Connection failed: " . $connection ->connect_error);
+	}
+	
+	?>
     <header class = "headeri">Uuden työntekijän tiedot</header>
 		<div class = "container">
     <p class = "pakolliset"> * Pakolliset tekstikentät </p>
@@ -147,11 +174,11 @@
 			<legend class = "legendi">Henkilötiedot</legend>
 				<div class = "sectioni1">
 					<label for = "Etunimi" >Etunimi <span class = "Tähti"> &nbsp * <?php echo $etunimiError;?> </span></label> <br>
-          <input id = "Etunimi" type="text" name = "etunimi">
+          <input id = "Etunimi" type="text" name = "etunimi" value = "<?php echo $etunimi;?>">
 				</div>
         <div class = "sectioni2">
           <label for = "Sukunimi" >Sukunimi <span class = "Tähti"> &nbsp *  <?php echo $sukunimiError;?> </span></label> <br>
-					<input id = "Sukunimi" type="text" name = "sukunimi">
+					<input id = "Sukunimi" type="text" name = "sukunimi" value = "<?php echo $sukunimi;?>">
         </div>
         <div class = "sectioni3">
           <label for = "Mies" >Sukupuoli <span class = "Tähti"> &nbsp * </span></label> <br>
@@ -162,21 +189,21 @@
         <div>
 				<div class = "sectioni4">
           <label for = "Puhelinnumero" >Puhelinnumero <span class = "Tähti"> &nbsp * <?php echo $puhelinnumeroError;?></span></label> <br>
-					<input id = "Puhelinnumero" type="text" name = "puhelinnumero">
+					<input id = "Puhelinnumero" type="text" name = "puhelinnumero" value = "<?php echo $puhelinnumero;?>">
 				</div>
         <div class = "sectioni5">
-          <label for = "Sposti" >Sähköposti</label> <br>
-					<input id = "Sposti" type="text" name = "sähköposti">
+          <label for = "Sposti" >Sähköposti <span class = "Tähti"> &nbsp * <?php echo $sähköpostiError;?></span></label> <br>
+					<input id = "Sposti" type="text" name = "sähköposti" value = "<?php echo $sähköposti;?>">
         </div>
       </div>
       <div>
 				<div class = "sectioni6">
           <label for = "Hetu" >Henkilötunnus <span class = "Tähti"> &nbsp * <?php echo $henkilötunnusError;?></span></label> <br>
-					<input id = "Hetu" type="text" name = "henkilötunnus">
+					<input id = "Hetu" type="text" name = "henkilötunnus" value = "<?php echo $henkilötunnus;?>">
 				</div>
         <div class = "sectioni7">
         <label for = "Asuinpaikka" >Asuinpaikka <span class = "Tähti"> &nbsp * <?php echo $asuinpaikkaError;?></span></label> <br>
-        <input id = "Asuinpaikka" type="text" name = "asuinpaikka">
+        <input id = "Asuinpaikka" type="text" name = "asuinpaikka" value = "<?php echo $asuinpaikka;?>">
       </div>
     </div>
 		</fieldset>
@@ -184,21 +211,21 @@
 			<legend class = "legendi">Työtiedot</legend>
 				<div class = "sectioni8">
           <label for = "Työnimike">Työnimike <span class = "Tähti"> &nbsp * <?php echo $työnimikeError;?></span></label> <br>
-          <input id = "Työnimike" type="text" name = "työnimike">
+          <input id = "Työnimike" type="text" name = "työnimike" value = "<?php echo $työnimike;?>">
 				</div>
         <div class = "sectioni9">
           <label for = "Esimies">Esimies <span class = "Tähti"> &nbsp * <?php echo $esimiesError;?></span></label> <br>
-          <input id = "Esimies" type="text" name = "esimies">
+          <input id = "Esimies" type="text" name = "esimies" value = "<?php echo $esimies;?>">
         </div>
         <div>
 				<div class = "sectioni10">
           <label for = "Sijainti">Sijainti <span class = "Tähti"> &nbsp * <?php echo $sijaintiError;?></span></label> <br>
-          <input id = "Sijainti" type="text" name = "sijainti">
+          <input id = "Sijainti" type="text" name = "sijainti" value = "<?php echo $sijainti;?>">
 				</div>
         <div class = "sectioni11">
           <label for = "Työsuhde">Työsuhde</label> <br>
 						<select id = "Työsuhde" name="työmuodot">
-							<option value="kokoaikainen">Kokoaikainen</option>
+							<option value="Kokoaikainen">Kokoaikainen</option>
 							<option value = "Määräaikainen" <?php if(isset($työsuhde) && $työsuhde == 'Määräaikainen')
 							echo 'selected = "selected"';
 							?>>Määräaikainen</option>
@@ -208,11 +235,11 @@
       <div>
 			<div class = "sectioni12">
           <label for = "Aloituspäivämäärä">Aloituspvm <span class = "Tähti2"> &nbsp *  <?php echo $aloituspvmError;?></span></label> <br>
-          <input id = "Aloituspäivämäärä" type="text" name = "aloituspäivämäärä">
+          <input id = "Aloituspäivämäärä" type="text" name = "aloituspäivämäärä" value = "<?php echo $aloituspvm;?>">
 				</div>
         <div class = "sectioni13">
           <label for = "Päättymispäivämäärä">Päättymispvm <span class = "Tähti2"> &nbsp *  <?php echo $päättymispvmError;?></span></label> <br>
-          <input id = "Päättymispäivämäärä" type="text" name = "päättymispäivämäärä">
+          <input id = "Päättymispäivämäärä" type="text" name = "päättymispäivämäärä" value = "<?php echo $päättymispvm;?>">
         </div>
       </div>
 		</fieldset>
